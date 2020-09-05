7163e3ba92f85dd368b8bd8d38c0dc046d7a8853
sebastian199912 <jruedar@unbosque.edu.co>
2020-09-05 15:00:24 -0500

    registrarse y registro completo

--- modelo/insertarEmpresa.php
+++ modelo/insertarEmpresa.php
@@ -7,10 +7,11 @@ $descripccion = NULL;
 $cod_empresa_t = 1;
 $estado_empresa =NULL;
 $camara_comercio=NULL;
+$tipo_de_empresa= null;
 
 
 $elUsr = "root";
-$elPw = "12345678";
+$elPw = "";
 $elServer = "localhost:3306";
 $laBd = "feriaop_bd";
 
@@ -18,7 +19,7 @@ $laBd = "feriaop_bd";
 $conexion = mysqli_connect($elServer, $elUsr, $elPw, $laBd);
 
 
-$sql = "INSERT INTO MATERIALES VALUES ($cod_material,'$nom_material')";
+$sql = "INSERT INTO empresa VALUES ('$nom_empresa','$nom_comercial','$descripccion',$cod_empresa_t,'$tipo_de_empresa','$estado_empresa',$camara_comercio)";
 $resultado = mysqli_query($conexion, $sql);
 mysqli_close($conexion);
 header("Location: ../vista/Material.php");
