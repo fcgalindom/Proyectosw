59a801ce8cb0f1e4ce06485258c64aba242d7048
YeffersonLinares <ylinares@unbosque.edu.co>
2020-09-13 16:21:00 -0500

    Llaves foraneas y primarias puestas para hoja de vida

--- controlador/hoja_vida_registro/CRUD_Estudiante.php
+++ controlador/hoja_vida_registro/CRUD_Estudiante.php
@@ -1,12 +1,12 @@
 <?php
 
 include("../../modelo/conexion.php");
-$key = $_POST['key'];
 
 //finalizado
 function guardarPerfil()
 {
     $perfil = $_POST['perfil'];
+    $key = $_POST['key'];
     $sql = "INSERT INTO PERFIL VALUES($key,'$perfil')";
     $conexion = conectar();
     mysqli_query($conexion,$sql);
@@ -18,12 +18,13 @@ function guardarFormacionAcademica()
 {
     #include("../../modelo/conexion.php");
     $conexion = conectar();
+    $key = $_POST['key'];
     $ins_bachiller  = $_POST['ins_bachiller'];
     $titulo = $_POST['titulo_bachiller'];
     $profesion = $_POST['profesion'];
     $ingles = $_POST['ingles'];
     $fecha = $_POST['fecha_grado'];
-    $sql = "INSERT INTO FORMACION_ACADEMICA VALUES(0,'$ins_bachiller','$titulo','$fecha','$profesion','$ingles')";
+    $sql = "INSERT INTO FORMACION_ACADEMICA VALUES($key,'$ins_bachiller','$titulo','$fecha','$profesion','$ingles')";
     mysqli_query($conexion,$sql);
     mysqli_close($conexion);
 }
@@ -31,11 +32,12 @@ function guardarFormacionAcademica()
 function guardarExperienciaLaboral()
 {
     $conexion = conectar();
+    $key = $_POST['key'];
     $cargo = $_POST['cargo'];
     $empresa = $_POST['empresa'];
     $funciones = $_POST['fun_principales'];
     $fecha_fin = $_POST['fecha_fin_exp_laboral'];
-    $sql = "INSERT INTO EXP_LABORAL VALUES(0,'$cargo','$empresa','$funciones','$fecha_fin',1)";
+    $sql = "INSERT INTO EXP_LABORAL VALUES(0,'$cargo','$empresa','$funciones','$fecha_fin',$key)";
     #echo $sql;
     mysqli_query($conexion,$sql);
     mysqli_close($conexion);
@@ -44,10 +46,11 @@ function guardarExperienciaLaboral()
 function guardarExperienciaAcademica()
 {
     $conexion = conectar();
+    $key = $_POST['key'];
     $nombre = $_POST['nom_exp_academica'];
     $conocimientos = $_POST['con_aplicados'];
     $materia = $_POST['materia'];
-    $sql = "INSERT INTO EXP_ACADEMICA VALUES(0,'$nombre','$conocimientos','$materia',1)";
+    $sql = "INSERT INTO EXP_ACADEMICA VALUES(0,'$nombre','$conocimientos','$materia',$key)";
     mysqli_query($conexion,$sql);
     mysqli_close($conexion);
 }
@@ -56,10 +59,11 @@ function guardarExperienciaAcademica()
 function guardarReferencias()
 {
     $conexion = conectar();
+    $key = $_POST['key'];
     $nombre = $_POST['nom_referencia'];
     $cargo = $_POST['cargo_referencia'];
     $celular = $_POST['celular_referencia'];
-    $sql = "INSERT INTO REFERENCIA VALUES(0,'$nombre','$cargo',$celular,1)";
+    $sql = "INSERT INTO REFERENCIA VALUES(0,'$nombre','$cargo',$celular,$key)";
     mysqli_query($conexion,$sql);
     mysqli_close($conexion);
 }
@@ -68,11 +72,12 @@ function guardarReferencias()
 function guardar_f_tecnica()
 {
     $conexion = conectar();
+    $key = $_POST['key'];
     $nombre = $_POST['nom_tecnico'];
     $fecha = $_POST['fec_tecnico'];
     $institucion = $_POST['institucion'];
     $nivel =  1;
-    $sql = "INSERT INTO F_TECNICA VALUES(0,'$nombre','$fecha','$institucion',$nivel,1)";
+    $sql = "INSERT INTO F_TECNICA VALUES(0,'$nombre','$fecha','$institucion',$nivel,$key)";
     mysqli_query($conexion,$sql);
     mysqli_close($conexion);
 }
@@ -81,17 +86,18 @@ function guardar_f_tecnica()
 function guardarCertificaciones()
 {
     $conexion = conectar();
+    $key = $_POST['key'];
     $nombre_cer = $_POST['nombre_certificacion'];
     $ins_certificado = $_POST['ins_certificado'];
     $fec_certificado = $_POST['fec_certificado'];
     $horas = $_POST['cant_horas'];
-    $sql = "INSERT INTO CERTIFICACION VALUES(0,'$nombre_cer','$fec_certificado','$ins_certificado',$horas,1)";
+    $sql = "INSERT INTO CERTIFICACION VALUES(0,'$nombre_cer','$fec_certificado','$ins_certificado',$horas,$key)";
     #echo $sql;
     mysqli_query($conexion,$sql);
     mysqli_close($conexion);
 }
 
-#guardarPerfil();
+guardarPerfil();
 #guardarFormacionAcademica();
 #guardar_f_tecnica();
 #guardarCertificaciones();
